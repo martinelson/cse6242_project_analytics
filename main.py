2b43f3f7db9ef685de5ecc8b1f276ecc4e2fefea
Martin <martin.c.nelson8@gmail.com>
2021-04-02 02:40:21 -0500

    refactor main add regression test files

--- main.py
+++ main.py
@@ -1,72 +1,43 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn import linear_model
 import statsmodels.api as sm
+import lintest
 
 
-# creating scaling and regression model variables
-model = linear_model.LinearRegression()
-train_split = 0.7
-test_split = 0.3
+def trip_regression(train_split, df, select_cols, dummy_cols, y_col, random_filter):
+    # reading in dataset and dropping columns
 
-# reading in dataset and dropping columns
-df = pd.read_csv("trips_summary_sql.csv")
+    df = df[select_cols]
+    print(df.head())
+    # categorical / dummy variables
+    df = pd.get_dummies(df, columns=dummy_cols, drop_first=True)
 
-drop_cols = ['date', 'year', 'week_year', 'day_year', 'count', 'fare_tot', 'tip_tot', 'additional_tot', 'trip_total_tot']
-df.drop(drop_cols, axis=1, inplace=True)
+    # splitting data
+    np.random.seed(0)
 
-# categorical / dummy variables
-dummy_cols = ['month', 'week_day', 'hour', 'pickup_community_area']
-df = pd.get_dummies(df, columns=dummy_cols, drop_first=True)
+    if random_filter:
+        df_train, df_test = train_test_split(df, train_size=train_split, test_size=1-train_split, random_state=100)
 
+        y_train = df_train[y_col]
+        x_train = df_train.drop(y_col, axis=1)
+        y_test = df_test[y_col]
+        x_test = df_test.drop(y_col, axis=1)
 
-# splitting data
-np.random.seed(0)
-df_train, df_test = train_test_split(df, train_size=train_split, test_size=test_split, random_state=100)
+    else:
+        y = df[y_col]
+        x = df.drop(y_col, axis=1)
 
+        x_train = x[:int(x.shape[0] * train_split)]
+        x_test = x[int(x.shape[0] * train_split):]
+        y_train = y[:int(x.shape[0] * train_split)]
+        y_test = y[int(x.shape[0] * train_split):]
 
-y_train = pd.DataFrame(df_train["total_revenue"])
-x_train = df_train.drop('total_revenue', axis=1)
-y_test = pd.DataFrame(df_test["total_revenue"])
-x_test = df_test.drop('total_revenue', axis=1)
+    # fit model via sm
+    model = sm.OLS(y_train, x_train)
+    results = model.fit()
+    print(results.summary())
 
-# vif of trip_miles over 86
-x_train = x_train.drop('trip_miles_tot', axis=1)
-x_test = x_test.drop('trip_miles_tot', axis=1)
-
-# vif of trip_seconds over 10
-x_train = x_train.drop('trip_seconds_tot', axis=1)
-x_test = x_test.drop('trip_seconds_tot', axis=1)
-
-
-# # fit the model
-reg = model.fit(x_train, y_train)
-coefs = reg.coef_
-print(coefs)
-intercept = reg.intercept_
-print(intercept)
-score = reg.score(x_train, y_train)
-print(score)
-# prediction = reg.predict(x_test)
-
-# fit model via sm
-model = sm.OLS(y_train, x_train)
-results = model.fit()
-print(results.params)
-print(results.summary())
-
-# LEFT TODO:
-# plot error distribution
-
-
-
-
-
-
-
-
-
-
-
+    lintest.linearity_test(model, y_train, 2000)
+    return model, results, x_train, y_train
 
